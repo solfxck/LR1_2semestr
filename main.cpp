7699d3b5b62a927f637cb3240be3111ff25e0ab6
solfxck <gleb.naymof@gmail.com>
2024-03-25 10:37:11 +0700

    Проверка на отрицательное число

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 void calculRectangle(double side1, double side2) {
+    if (side1 <= 0 || side2 <= 0) {
+        cout << "error";
+        return;
+    }
     double perimetr = (side1 + side2) * 2; //Периметр
     double area = side1 * side2; //Площадь 
     double diagonal = sqrt(side1 * side1 + side2 * side2); //Длина диагонали
@@ -13,5 +17,5 @@ void calculRectangle(double side1, double side2) {
 }
 
 int main(){
-    calculRectangle(5, 6);
+    calculRectangle(5, -1);
 }
